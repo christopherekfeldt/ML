5d8992034c12a45d6eeb7d2b6e5d933fb0c9a201
christopherekfeldt <christopher.ekfeldt@hotmail.se>
2020-10-05 22:08:55 +0200

    Start of dataset

---
+++ ml.py
@@ -0,0 +1,35 @@
+# Load libraries
+from pandas import read_csv
+from pandas.plotting import scatter_matrix
+from matplotlib import pyplot
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+# Load dataset
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = read_csv(url, names=names)
+
+'''
+# rows/columns in dataset
+print(dataset.shape)
+
+# first 20 rows of the data:
+print(dataset.head(20))
+
+# summary count, mean, the min and max values as well as some percentiles.
+print(dataset.describe())
+
+# class distribution
+print(dataset.groupby('class').size())
+'''
